0136c7ab19701965466194c498b2b0a9edb1d8d2
Priyan Indika <priyan_ik@hotmail.com>
2024-07-03 15:21:46 +1200

    Number formatting and Instruction Document.

--- SmartlyCodingExercise/SmartlyCodingExercise.Web/Models/EmployeeSalaryDetails.cs
+++ SmartlyCodingExercise/SmartlyCodingExercise.Web/Models/EmployeeSalaryDetails.cs
@@ -12,18 +12,18 @@ namespace SmartlyCodingExercise.Web.Models
         
         public decimal GrossIncome { get; set; }
         [DisplayName("Gross Income")]
-        public string GrossIncomeDisplay => $"{StringConstantsHelpers.Currency}{GrossIncome}";
+        public string GrossIncomeDisplay => $"{StringConstantsHelpers.Currency}{string.Format("{0:0.00}", GrossIncome)}";
 
         public decimal IncomeTax { get; set; }
         [DisplayName("Income Tax")]
-        public string IncomeTaxDisplay => $"{StringConstantsHelpers.Currency}{IncomeTax}";
+        public string IncomeTaxDisplay => $"{StringConstantsHelpers.Currency}{string.Format("{0:0.00}", IncomeTax)}";
 
         public decimal NetIncome { get; set; }
         [DisplayName("Net Income")]
-        public string NetIncomeDisplay => $"{StringConstantsHelpers.Currency}{NetIncome}";
+        public string NetIncomeDisplay => $"{StringConstantsHelpers.Currency}{string.Format("{0:0.00}", NetIncome)}";
 
         public decimal SuperAmount { get; set; }
         [DisplayName("Super")]
-        public string SuperAmountDisplay => $"{StringConstantsHelpers.Currency}{SuperAmount}";
+        public string SuperAmountDisplay => $"{StringConstantsHelpers.Currency}{string.Format("{0:0.00}", SuperAmount)}";
     }
 }
